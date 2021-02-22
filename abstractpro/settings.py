32f422030c537c4e8d3323ec8920a637604adbc8
Manoja Kuamr Panda <manojakumarpanda@momenttext>
2021-02-22 21:17:29 +0530

    third commit

--- abstractpro/settings.py
+++ abstractpro/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILE_DIRS=os.path.join(BASE_DIR,'static/files')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/'
